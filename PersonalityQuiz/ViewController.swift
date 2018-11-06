b7bc0fa8a77fc0c6f8a3718ae7b06cf9394f307d
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-11-05 19:35:56 -0500

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Arvin Zojaji on 2018-11-05.
+//  Copyright © 2018 Arvin Zojaji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
